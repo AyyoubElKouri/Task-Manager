d1a11e2c37ae4b70cb9ee65ba4d0088bed83e4e4
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2025-07-31 20:39:31 +0100

    chore(Pages): refactoring pages (move TaskDashboard implementation to tasks feature

---
+++ src/features/tasks/pages/TaskDashboard.tsx
@@ -0,0 +1,58 @@
+import { useStatistics } from "@/hooks/useStatistics";
+import StatGroup from "../components/StatGroup";
+import StatItem from "../components/StatItem";
+import TaskList from "../components/TaskList";
+import { TaskService } from "@/services/tasks/TaskService";
+import { useEffect } from "react";
+import { Logger } from "@/helpers/logger";
+import ActionButtons from "../components/ActionButtons";
+
+const logger = new Logger("Task Dashboard", "TaskDashboard.tsx", "tasks");
+
+const TaskDashboard = () => {
+    useEffect(() => {
+        try {
+            TaskService.getInstance().initialize();
+        } catch (error: any) {
+            logger.error(error, 19);
+        }
+    }, []);
+
+    const {
+        numberOfTasks,
+        numberOfCompletedTasks,
+        numberOfHours,
+        numberOfCompletedHours,
+        rateOfCompletion,
+    } = useStatistics();
+
+    return (
+        <div>
+            <div className="flex justify-center flex-col items-center p-6 gap-4">
+                {/* === Statistics Section === */}
+                <StatGroup>
+                    <StatItem label="All Tasks" value={numberOfTasks} />
+                    <StatItem
+                        label="Completed Tasks"
+                        value={numberOfCompletedTasks}
+                    />
+                    <StatItem label="Total Hours" value={numberOfHours} />
+                    <StatItem
+                        label="Total Completed Hours"
+                        value={numberOfCompletedHours}
+                    />{" "}
+                    <StatItem
+                        label="Rate of Completione"
+                        value={rateOfCompletion}
+                    />
+                </StatGroup>
+
+                {/* === Tasks Section === */}
+                <TaskList />
+                <ActionButtons />
+            </div>
+        </div>
+    );
+};
+
+export default TaskDashboard;
--- src/pages/TaskDashboard.tsx
+++ src/pages/TaskDashboard.tsx
@@ -1,60 +1,7 @@
-import { useEffect } from "react";
-import StatGroup from "../features/tasks/components/StatGroup";
-import StatItem from "../features/tasks/components/StatItem";
-import TaskList from "@/features/tasks/components/TaskList";
-import { useStatistics } from "../hooks/useStatistics";
-import { TaskService } from "../services/tasks/TaskService";
-import { Logger } from "@/helpers/logger";
+import TaskDashboard from "@/features/tasks/pages/TaskDashboard";
 
-/**
- * Global Logger.
- */
-const logger = new Logger("Task Dashboard", "TaskDashboard.tsx", "pages");
-
-const TaskDashboard = () => {
-    useEffect(() => {
-        try {
-            TaskService.getInstance().initialize();
-        } catch (error: any) {
-            logger.error(error, 19);
-        }
-    }, []);
-
-    const {
-        numberOfTasks,
-        numberOfCompletedTasks,
-        numberOfHours,
-        numberOfCompletedHours,
-        rateOfCompletion,
-    } = useStatistics();
-
-    return (
-        <div>
-            <div className="flex justify-center flex-col items-center p-6 gap-4">
-                {/* === Statistics Section === */}
-                <StatGroup>
-                    <StatItem label="All Tasks" value={numberOfTasks} />
-                    <StatItem
-                        label="Completed Tasks"
-                        value={numberOfCompletedTasks}
-                    />
-                    <StatItem label="Total Hours" value={numberOfHours} />
-                    <StatItem
-                        label="Total Completed Hours"
-                        value={numberOfCompletedHours}
-                    />{" "}
-                    <StatItem
-                        label="Rate of Completione"
-                        value={rateOfCompletion}
-                    />
-                </StatGroup>
-
-                {/* === Tasks Section === */}
-                <TaskList />
-                <h1 className="bg-background-blue rounded-corner text-white text-3xl font-exo2">Hello world</h1>
-            </div>
-        </div>
-    );
+const TaskDashboardPage = () => {
+    return <TaskDashboard />;
 };
 
-export default TaskDashboard;
+export default TaskDashboardPage;
